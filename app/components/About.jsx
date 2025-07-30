f6bded8a16c7ad529ec86a71dad3c7e837a68bb2
Javier <javier.caceresl@usm.cl>
2025-07-30 18:32:37 -0400

    Add Contact and Footer - Modify About

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,11 +3,11 @@ import { assets, infoList, toolsData } from '@/assets/assets'
 
 const About = () => {
   return (
-    <div id='about' className='w-full px-[12%] py-10 scroll-mt-15'>
+    <div id='about' className='w-full px-[12%] py-10 scroll-mt-20'>
       <h4 className='text-center mb-2 text-lg font-Ovo'>Introduction</h4>
       <h2 className='text-center text-5xl font-Ovo'>About Me</h2>
 
-      <div className='flex w-full flex-col lg:flex-row items-center gap-20 mt-12'>
+      <div className='flex w-full flex-col lg:flex-row items-center gap-20 mt-10'>
         <div className='w-64 sm:w-80 rounded-3xl max-w-none'>
             <Image src={assets.user_image_3} alt='user' className='w-full rounded-3xl' />
         </div>
--- app/page.js
+++ app/page.js
@@ -4,6 +4,8 @@ import Header from "./components/Header";
 import About from "./components/About";
 import Projects from "./components/Projects";
 import Experience from "./components/Experience";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 export default function Home() {
   return (
@@ -13,6 +15,8 @@ export default function Home() {
     <About />
     <Projects />
     <Experience />
+    <Contact />
+    <Footer />
     </>
   );
 }

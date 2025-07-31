f84fb702519791246a98a204ed345489046c5d18
Javier <javier.caceresl@usm.cl>
2025-07-31 02:21:17 -0400

    Add animations

--- app/components/Projects.jsx
+++ app/components/Projects.jsx
@@ -1,17 +1,43 @@
 import React, { useContext } from 'react'
 import Image from 'next/image'
 import { assets, serviceData } from '@/assets/assets'
+import { motion } from "motion/react"
 
 const Projects = () => {
     return (
-        <div id="projects" className='w-full px-[12%] py-10 scroll-mt-20'>
-            <h4 className='text-center mb-2 text-lg font-Ovo'>My Work</h4>
-            <h2 className='text-center text-5xl font-Ovo'>Projects</h2>
-            <p className='text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo'>Here are some areas where I have developed projects.</p>
+        <motion.div
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 1 }}
+        id="projects" className='w-full px-[12%] py-10 scroll-mt-20'>
+
+            <motion.h4
+            initial={{ y:-20, opacity:0 }}
+            whileInView={{ y:0, opacity:1 }}
+            transition={{ delay: 0.3, duration: 0.5 }}
+            className='text-center mb-2 text-lg font-Ovo'>My Work</motion.h4>
+
+            <motion.h2
+            initial={{ opacity:0 }}
+            whileInView={{ opacity:1 }}
+            transition={{ delay: 0.7, duration: 0.5 }}
+            className='text-center text-5xl font-Ovo'>Projects</motion.h2>
             
-            <div className='grid grid-cols-auto gap-6 my-10'>
+            <motion.p
+            initial={{ y:-20, opacity:0 }}
+            whileInView={{ y:0, opacity:1 }}
+            transition={{ delay: 0.3, duration: 0.5 }}
+            className='text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo'>Here are some areas where I have developed projects.</motion.p>
+
+            <motion.div
+            initial={{ opacity:0 }}
+            whileInView={{ opacity:1 }}
+            transition={{ delay: 0.9, duration: 0.6 }}
+            className='grid grid-cols-auto gap-6 my-10'>
                 {serviceData.map(({icon, title, description, link}, index)=>(
-                    <div key={index} className='border border-gray-400 rounded-lg px-8 py-12 hover:shadow-black cursor-pointer hover:bg-lightHover hover:-translate-y-1 duration-500 dark:hover:bg-darkHover dark:hover:shadow-white'>
+                    <motion.div
+                    whileHover={{ scale: 1.05 }}
+                    key={index} className='border border-gray-400 rounded-lg px-8 py-12 hover:shadow-black cursor-pointer hover:bg-lightHover hover:-translate-y-1 duration-500 dark:hover:bg-darkHover dark:hover:shadow-white'>
                         <Image src={icon} alt='' className='w-10'/>
                         <h3 className='text-lg my-4 text-gray-700 dark:text-white'>{title}</h3>
                         <p className='text-gray-600 text-sm leading-5 dark:text-white/80'>
@@ -20,10 +46,10 @@ const Projects = () => {
                         <a href={link} className='flex items-center gap-2 text-sm mt-5'>
                             Read more <Image src={assets.right_arrow} alt='' className='w-4' />
                         </a>
-                    </div>
+                    </motion.div>
                 ))}
-            </div>
-        </div>
+            </motion.div>
+        </motion.div>
     )
 }
 
